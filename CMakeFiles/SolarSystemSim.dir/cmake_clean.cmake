5bc1257b2a7b41483ec05c984c8210fdd849fc3f
mzuzuguy <preciousmusole18@gmail.com>
2026-07-13 20:56:31 +0200

    initial commit

---
+++ CMakeFiles/SolarSystemSim.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SolarSystemSim.dir/link.d"
+  "CMakeFiles/SolarSystemSim.dir/main.cpp.o"
+  "CMakeFiles/SolarSystemSim.dir/main.cpp.o.d"
+  "SolarSystemSim"
+  "SolarSystemSim.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/SolarSystemSim.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
